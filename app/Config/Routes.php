601367b071f8b17837e850bae0b0ff09cb8755be
Bacay, Sherwin <bacaylabog@gmail.com>
2023-09-21 18:12:12 +0800

    routing

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,5 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('/products', 'ProductsController::Bacay');
+$routes->get('/products/(:any)', 'ProductsController::products/$1');
---
+++ app/Controllers/ProductsController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class ProductsController extends BaseController
+{
+    public function products($products)
+    {
+        echo $products;
+    }
+    public function Bacay()
+    {
+        echo 'ski';
+    }
+    public function index()
+    {
+        //
+    }
+    
+}
